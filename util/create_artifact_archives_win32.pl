ad5cb4b0b02ac1c07630fb571ff2cfca4896525b
iPherian <iPherian@users.noreply.github.com>
2018-03-18 11:20:30 -0700

    don't copy dlls in plain config (dont exist)

--- util/create_artifact_archives_win32.pl
+++ util/create_artifact_archives_win32.pl
@@ -15,14 +15,21 @@ foreach my $arch_base ("$archive_tmp_path/all", "$archive_tmp_path/min") {
 }
 
 my ($obj_out_path, $include_out_path, $artifact_zip_min_name,
-    $artifact_zip_all_name) = @ENV{qw( OBJ_OUT_PATH INCLUDE_OUT_PATH
-                                       ARTIFACT_ZIP_MIN_NAME
-                                       ARTIFACT_ZIP_ALL_NAME )};
+    $artifact_zip_all_name, $configuration) =
+                         @ENV{
+                           qw( OBJ_OUT_PATH
+                               INCLUDE_OUT_PATH
+                               ARTIFACT_ZIP_MIN_NAME
+                               ARTIFACT_ZIP_ALL_NAME
+                               CONFIGURATION    
+                           )
+                         };
+my $robocopy_opts = qq{/COPYALL /E /FP /NP};
 
 # create 'all' archive
-my_system(qq{robocopy "$obj_out_path" "$archive_tmp_path\\all\\bin" *.dll *.exe /COPYALL /E});
-my_system(qq{robocopy "$obj_out_path" "$archive_tmp_path\\all\\lib" *.lib /COPYALL /E});
-my_system(qq{robocopy "$include_out_path" "$archive_tmp_path\\all\\include" * /COPYALL /E});
+my_system(qq{robocopy "$obj_out_path" "$archive_tmp_path\\all\\bin" *.dll *.exe $robocopy_opts});
+my_system(qq{robocopy "$obj_out_path" "$archive_tmp_path\\all\\lib" *.lib $robocopy_opts});
+my_system(qq{robocopy "$include_out_path" "$archive_tmp_path\\all\\include" * $robocopy_opts});
 chdir "$archive_tmp_path/all";
 my_system(qq{7z a "..\\..\\$artifact_zip_all_name" "*"});
 chdir "..\\..";
@@ -31,10 +38,14 @@ chdir "..\\..";
 foreach my $path ('ssleay32.lib', 'libeay32.lib') {
   my_system(qq{copy "$obj_out_path\\$path" "$archive_tmp_path\\min\\lib"});
 }
-foreach my $path ('ssleay32.dll', 'libeay32.dll', 'openssl.exe') {
+my @copy_for_min_bin = ('openssl.exe');
+if ($configuration =~ /shared/i) {
+  push @copy_for_min_bin, 'ssleay32.dll', 'libeay32.dll';
+}
+foreach my $path (@copy_for_min_bin) {
   my_system(qq{copy "$obj_out_path\\$path" "$archive_tmp_path\\min\\bin"});
 }
-my_system(qq{robocopy "$include_out_path" "$archive_tmp_path\\min\\include" * /COPYALL /E});
+my_system(qq{robocopy "$include_out_path" "$archive_tmp_path\\min\\include" * $robocopy_opts});
 chdir "$archive_tmp_path/min";
 my_system(qq{7z a "..\\..\\$artifact_zip_min_name" "*"});
 chdir "..\\..";
